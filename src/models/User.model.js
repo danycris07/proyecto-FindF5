166250bd2d03a030c6d9d4928e68fc30f17485be
Cristaldo Jonathan <cristaldojonathandaniel2021@gmail.com>
2026-08-20 10:20:44 -0300

    Feat: se añadio UserModel

---
+++ src/models/User.model.js
@@ -0,0 +1,32 @@
+import { sequelize } from "../config/database.js";
+const { DataTypes } = sequelize;
+
+export const user = sequelize.define(
+  "User",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("PLAYER", "FIELD_OWNER", "ADMIN"),
+      defaultValue: "PLAYER",
+    },
+  },
+  {
+    paranoid: true,
+  },
+);
