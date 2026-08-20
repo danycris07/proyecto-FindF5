0d3d0996653b31bd88a62b87c3b21aa97e66d121
Cristaldo Jonathan <cristaldojonathandaniel2021@gmail.com>
2026-08-20 10:20:02 -0300

    Feat: se añadio TeamModel

---
+++ src/models/Team.model.js
@@ -0,0 +1,32 @@
+import { sequelize } from "../config/database.js";
+const { DataTypes } = sequelize;
+
+const Team = sequelize.define(
+  "Team",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    captainId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+  },
+  {
+    paranoid: true,
+  },
+);
